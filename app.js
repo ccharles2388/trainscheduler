fdde7716b5edd40c1bff41c87edf0ebd3e8942ea
Curtis Charles <ccharles2388@gmail.com>
2019-09-19 17:53:38 -0400

    created firebase database

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 //1. Initialize Firebase
 var config = {
-  apiKey: "AIzaSyDdPzvFCDhjbEvJyDlNv0CYok-qUtJc_YI",
-  authDomain: "time-sheet-55009.firebaseapp.com",
-  databaseURL: "https://time-sheet-55009.firebaseio.com",
-  storageBucket: "time-sheet-55009.appspot.com"
+  apiKey: "AIzaSyDxPZb4HtuB71w2elqnYerNFHOCVUEYkb0",
+  authDomain: "myfirebaseproject-66a8e.firebaseapp.com",
+  databaseURL: "https://myfirebaseproject-66a8e.firebaseio.com",
+  projectId: "myfirebaseproject-66a8e",
+  storageBucket: "",
+  messagingSenderId: "891611664308",
+  appId: "1:891611664308:web:5cdf0c9e115c35a9a02ce1"
 };
 
 firebase.initializeApp(config);
