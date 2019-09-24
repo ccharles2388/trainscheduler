15b45bc087d26808d75fe9749bdac2d06073c006
Curtis Charles <ccharles2388@gmail.com>
2019-09-24 10:40:05 -0400

    intial js

--- app.js
+++ app.js
@@ -12,3 +12,81 @@ var config = {
 firebase.initializeApp(config);
 
 var database = firebase.database();
+
+// 2. Button for adding Employees
+$("#add-train-btn").on("click", function(event) {
+  event.preventDefault();
+
+  // Grabs user train input
+  var trainName = $("#train-name-input").val().trim();
+  var trainDest = $("#destination-input").val().trim();
+  var firstTime = moment($("#first-time-input").val().trim(), "MM/DD/YYYY").format("X");
+  var frequency = $("#frequency-input").val().trim();
+
+  // Creates local "temporary" object for holding train data
+  var newTrain = {
+    name: newTrain,
+    dest: trainDest,
+    time: firstTime,
+    freq: frequency
+  };
+
+  // Uploads train data to the database
+  database.ref().push(newTrain);
+
+  // Logs everything to console
+  console.log(newTrain.name);
+  console.log(newTrain.dest);
+  console.log(newTrain.time);
+  console.log(newTrain.freq);
+
+  alert("New Train Schdedule Successfully Added");
+
+  // Clears all of the text-boxes
+  $("#employee-name-input").val("");
+  $("#dest-input").val("");
+  $("#time-input").val("");
+  $("#freq-input").val("");
+});
+
+// 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+database.ref().on("child_added", function(childSnapshot) {
+  console.log(childSnapshot.val());
+
+  // Store everything into a variable.
+  var newTrain = childSnapshot.val().name;
+  var trainDest = childSnapshot.val().dest;
+  var firstTime = childSnapshot.val().time;
+  var frequency = childSnapshot.val().freq;
+
+  // Console Log Train Info
+  console.log(newTrain);
+  console.log(trainDest);
+  console.log(firstTime);
+  console.log(frequency);
+
+  // Prettify the Train Time For Mins time
+  var firstTimePretty = moment.unix(firstTime).format("HHMM");
+
+  // Calculate the remaining time
+  
+
+
+  // Create the new row
+  var newRow = $("<tr>").append(
+    $("<td>").text(newTrain),
+    $("<td>").text(trainDest),
+    $("<td>").text(firstTimePretty),
+    $("<td>").text(empMonths),
+    $("<td>").text(frequency),
+    $("<td>").text(empBilled)
+  );
+
+  // Append the new row to the table
+  $("#train-table > tbody").append(newRow);
+});
+
+// Train Schedule Time Notes And Math Work
+// -----------------------------------------------------------------------------
+// FirstTimePretty Needs To Be Formated
+
