07ffa9da37d50fa449c8ff8199d0c0518b80cd3f
Curtis Charles <ccharles2388@gmail.com>
2019-09-24 12:00:45 -0400

    updated size

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var config = {
   authDomain: "myfirebaseproject-66a8e.firebaseapp.com",
   databaseURL: "https://myfirebaseproject-66a8e.firebaseio.com",
   projectId: "myfirebaseproject-66a8e",
+  // Missing Storage Bucket Information - Researching 
   storageBucket: "",
   messagingSenderId: "891611664308",
   appId: "1:891611664308:web:5cdf0c9e115c35a9a02ce1"
@@ -14,9 +15,9 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 // 2. Button for adding Employees
-$("#add-train-btn").on("click", function(event) {
+$("#add-train-btn").on("click", function (event) {
   event.preventDefault();
-
+  console.log("ok");
   // Grabs user train input
   var trainName = $("#train-name-input").val().trim();
   var trainDest = $("#destination-input").val().trim();
@@ -50,7 +51,7 @@ $("#add-train-btn").on("click", function(event) {
 });
 
 // 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
-database.ref().on("child_added", function(childSnapshot) {
+database.ref().on("child_added", function (childSnapshot) {
   console.log(childSnapshot.val());
 
   // Store everything into a variable.
@@ -69,7 +70,7 @@ database.ref().on("child_added", function(childSnapshot) {
   var firstTimePretty = moment.unix(firstTime).format("HHMM");
 
   // Calculate the remaining time
-  
+
 
 
   // Create the new row
